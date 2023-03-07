c39c9734030c3cae9078088dd338fc20dc7cf9b1
Vedant Gawande <programmer.vedant@gmail.com>
2023-03-07 16:08:51 +0530

    in learning phase

--- main.py
+++ main.py
@@ -1,9 +1,15 @@
 from fastapi import FastAPI
-import uvicorn
+# import uvicorn
+from routers import admin
+from fastapi.staticfiles import StaticFiles
 
 
 app = FastAPI()
 
+
+app.include_router(admin.router)
+
+app.mount("/static", StaticFiles(directory= "static"), name="static",)
 
 # if __name__ == '__main__':
 #     uvicorn.run(app,host='127.0.0.1')
--- routers/admin.py
+++ routers/admin.py
@@ -1,10 +1,15 @@
-from fastapi import APIRouter
+from fastapi import APIRouter,Request
+from fastapi.responses import HTMLResponse
+from fastapi.templating import Jinja2Templates
 
 
 router = APIRouter(
     prefix='/admin_'
 )
 
-@router.post('login')
-def login():
-    pass
+
+templates = Jinja2Templates(directory='templates')
+
+@router.get('login',response_class=HTMLResponse)
+def login(request:Request):
+    return templates.TemplateResponse('admin_login.html',{"request":request})
