2822f3d2873bc6930591580006ec5b2dacb358ac
Vedant Gawande <programmer.vedant@gmail.com>
2023-05-02 16:55:15 +0530

    5th time minor change and validate

--- routers/user.py
+++ routers/user.py
@@ -366,7 +366,8 @@ async def recommended_videos_to_user(request:Request,db:Session=Depends(get_db))
             videos = db.query(models.Videos).order_by(text("RANDOM()"))
     else:
         videos = db.query(models.Videos).order_by(text("RANDOM()"))
-    if user.recommendations:
+    print(user)
+    if user:
         recommended_videos = user.recommendations
     # videos = db.query(models.Videos).all()
     if recommended_videos:
